035fe3341a6045706b4c669d3c5359b6b1d7901d
Adam <32522043+GHFear@users.noreply.github.com>
2025-09-12 12:59:28 +0200

    0.1.2

---
+++ compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Make sure script stops on errors
+set -e
+
+# Navigate to emsdk
+cd emsdk
+
+# Source the Emscripten environment
+source ./emsdk_env.sh
+
+# Go back to the project root
+cd ..
+
+# Compile C++ to WebAssembly/JS
+emcc main.cpp \
+  -o index.js \
+  -s EXPORTED_FUNCTIONS='["_main","_unpack_buffer","_open_file","_get_unpacked_ptr","_get_unpacked_size","_malloc","_free"]' \
+  -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","HEAPU8"]' \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -O3 \
+  --bind
+
+echo "Build complete!"
+
