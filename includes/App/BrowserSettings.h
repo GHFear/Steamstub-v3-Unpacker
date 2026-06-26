bd11fad0ebbca5c67623de9f1d7b3416cbc0ea10
GHFear <ghfear@hotmail.com>
2026-06-26 14:49:52 +0200

    0.2.1 v2

--- includes/App/BrowserSettings.h
+++ includes/App/BrowserSettings.h
@@ -1,7 +1,12 @@
 #pragma once
 // ------------------------------------------------------------------------- //
 //  Self-contained SteamStub v3 unpacker By GHFear @ IllusorySoftware        //
-//  Object-oriented browser/settings access                                  //
+//  Compatible with Emscripten (drop the emsdk folder next to this file and  //
+//  compile with the included compile script for linux)                      //
+// ------------------------------------------------------------------------- //
+//  Lots of credit to Cyanic (aka Golem_x86), atom0s and illnyang for prior  //
+//  research on steamstub drm.                                               //
+//  Without y'all, this wouldn't be possible.                                //
 // ------------------------------------------------------------------------- //
 
 #include <string>
--- includes/Global/Global.h
+++ includes/Global/Global.h
@@ -1,7 +1,12 @@
 #pragma once
 // ------------------------------------------------------------------------- //
 //  Self-contained SteamStub v3 unpacker By GHFear @ IllusorySoftware        //
-//  Backwards-compatible access to object-oriented output buffers            //
+//  Compatible with Emscripten (drop the emsdk folder next to this file and  //
+//  compile with the included compile script for linux)                      //
+// ------------------------------------------------------------------------- //
+//  Lots of credit to Cyanic (aka Golem_x86), atom0s and illnyang for prior  //
+//  research on steamstub drm.                                               //
+//  Without y'all, this wouldn't be possible.                                //
 // ------------------------------------------------------------------------- //
 
 #include <cstdint>
